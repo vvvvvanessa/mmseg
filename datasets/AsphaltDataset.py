8bac66f2c92e0eabeab7c8e7afc195620133e8a9
vanessa <d.wang-1@utwente.nl>
2025-04-07 15:31:02 +0200

    config

--- datasets/AsphaltDataset.py
+++ datasets/AsphaltDataset.py
@@ -6,7 +6,7 @@ class AsphaltDataset(BaseSegDataset):
   METAINFO = dict(classes = ("background", "voids"), palette = [[0, 0, 0], [255, 0, 0]])
   def __init__(self, **kwargs):
     print("start to init")
-    super().__init__(ann_file = "ann_dir/train/", seg_map_suffix = ".JPG", **kwargs)
+    super().__init__(ann_file = "ann_dir/train/", seg_map_suffix = ".jpg", **kwargs)
     print(f'Loaded {len(self)} images')  # 打印数据集大小
     print("ann file = " + self.ann_file)#    print(self[0])
     print("data root" + self.data_root)
