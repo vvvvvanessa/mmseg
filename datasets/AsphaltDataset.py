ece062743131ae6eb2306bc17f24d8af1d3cd7ef
vanessa <d.wang-1@utwente.nl>
2025-04-14 11:22:30 +0200

    update

--- datasets/AsphaltDataset.py
+++ datasets/AsphaltDataset.py
@@ -6,9 +6,16 @@ class AsphaltDataset(BaseSegDataset):
   METAINFO = dict(classes = ("background", "voids"), palette = [[0, 0, 0], [255, 0, 0]])
   def __init__(self, **kwargs):
     print("start to init")
-    super().__init__(ann_file = "ann_dir/train/", seg_map_suffix = ".jpg", **kwargs)
+    super().__init__(ann_file = "ann_dir/train/", seg_map_suffix = ".png", **kwargs)
     print(f'Loaded {len(self)} images')  # 打印数据集大小
     print("ann file = " + self.ann_file)#    print(self[0])
     print("data root" + self.data_root)
     print("suffix" + self.seg_map_suffix)
+  #
+  # 在自定义 dataset 中 __getitem__ 加这个
+  def __getitem__(self, idx):
+    print(f"Loading sample {idx}")
+    return super().__getitem__(idx)
+
+
 
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os.path
 from mmengine import Config
 from mmengine.runner import Runner
+from mmseg.registry import DATASETS  # ✅ 这是官方推荐方式
+from mmengine.registry import MODELS
+
 import datasets.AsphaltDataset
-
-
+import cv2
+import numpy as np
 
 #
 # config_file = 'pspnet_r50-d8_4xb2-40k_cityscapes-512x1024.py'
@@ -29,13 +32,31 @@ import datasets.AsphaltDataset
 if __name__ == "__main__":
     # 加载配置文件
     config_path = 'configs/my_config.py'
-
-    # config_path.get('val_dataloader')
-    # config_path.get('test_dataloader')
-
     cfg = Config.fromfile(config_path)
+
     # 构建 Runner
     runner = Runner.from_cfg(cfg)
+
+    # 在替换前保存原始 train_step
+    orig_train_step = runner.model.train_step
+
+
+    def debug_train_step(self, data_batch, optim_wrapper):
+        print('🟢 Debug train_step called')
+        print(f'data_batch keys: {data_batch.keys()}')
+        inputs = data_batch['inputs']
+        print(f'inputs type: {type(inputs)}')
+        print(f'Number of samples: {len(inputs)}')
+        if isinstance(inputs, list) and len(inputs) > 0:
+            print(f'First input shape: {inputs[0].shape}')
+        data_samples = data_batch['data_samples']
+        print(f'data_samples type: {type(data_samples)}')
+        print(f'First gt_sem_seg shape: {data_samples[0].gt_sem_seg.data.shape}')
+        # 调用原始的 train_step 方法
+        return orig_train_step(data_batch, optim_wrapper)
+
+
+    runner.model.train_step = debug_train_step.__get__(runner.model, type(runner.model))
 
     # 开始训练
     runner.train()
